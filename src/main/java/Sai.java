d71652d363e47a1d253bf112492c6796aeedf312
jsreddy512 <jsreddy512@gmail.com>
2023-08-14 21:49:32 +0530

    new sai

---
+++ src/main/java/Sai.java
@@ -0,0 +1,7 @@
+public class Sai {
+    public static void main(String[] args) {
+        System.out.println("Saikiran Reddy");
+        System.out.println("nari");
+        System.out.println("gannu");
+    }
+}
