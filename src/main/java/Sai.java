5b9aa2c6c743df0a471884db7ad5544137556138
Saikiran Reddy <90782775+jsreddy512@users.noreply.github.com>
2023-08-14 22:04:32 +0530

    Update Sai.java

--- src/main/java/Sai.java
+++ src/main/java/Sai.java
@@ -2,9 +2,6 @@ public class Sai {
     public static void main(String[] args) {
         System.out.println("Saikiran Reddy");
         System.out.println("Nari");
-        System.out.println("Laxma");
-        System.out.println("Likith");
-        System.out.println("Anil");
         System.out.println("gannu");
     }
 }
